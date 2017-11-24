759a5b5acfdb67fd753965492aa7ab4ea3ae7a94
moh4med <moh4med.ahmed@gmail.com>
2017-11-24 14:23:37 +0200

    add threaded matrix multiplicatio per element

--- MatrixMul.c
+++ MatrixMul.c
@@ -1,23 +1,18 @@
-/*
- ============================================================================
- Name        : MatrixMul.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
 
 #include <stdio.h>
 #include <stdlib.h>
-int **a, **b, **c;
+int **a, **b, **c, **d;
 int n, m, r;
-#define MAXSIZE 5
+#define MAXSIZE 10
 #define MAXELEMENT 5
+struct data {
+	int i;
+	int j;
+};
 void initialize() {
-	n = 1+rand() % MAXSIZE;
-	m = 1+rand() % MAXSIZE;
-	r= 1+rand() % MAXSIZE;
+	n = 1 + rand() % MAXSIZE;
+	m = 1 + rand() % MAXSIZE;
+	r = 1 + rand() % MAXSIZE;
 	a = malloc(n * sizeof(int*));
 	for (int i = 0; i < n; ++i) {
 		a[i] = malloc(m * sizeof(int));
@@ -40,6 +35,10 @@ void initialize() {
 	for (int i = 0; i < n; ++i) {
 		c[i] = malloc(r * sizeof(int));
 	}
+	d = malloc(n * sizeof(int*));
+	for (int i = 0; i < n; ++i) {
+		d[i] = malloc(r * sizeof(int));
+	}
 
 }
 void nonThreadedMatMult() {
@@ -52,7 +51,7 @@ void nonThreadedMatMult() {
 		}
 	}
 }
-void printMatrix(int **x,int n,int m) {
+void printMatrix(int **x, int n, int m) {
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
 			printf("%d  ", x[i][j]);
@@ -61,12 +60,47 @@ void printMatrix(int **x,int n,int m) {
 	}
 	printf("\n");
 }
+void elementrunner(void *param) {
+	 struct data *p = param; // the structure that holds our data
+	   int sum = 0;
+	   //Row multiplied by column
+	   for (int k = 0; k < m; ++k) {
+		//printf("in thread:%d %d \n",p->i,p->j);
+		sum+=(a[p->i][k]*b[k][p->j]);
+	   }
+	   //assign the sum to its coordinate
+	   d[p->i][p->j] = sum;
+	   //Exit the thread
+	   pthread_exit(0);
+}
+void threadedMatMultPerElement() {
+	pthread_t id[n*r];
+	int cnt=0;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < r; ++j) {
+			struct data *p = (struct data *) malloc(sizeof(struct data));
+			p->i = i;
+			p->j = j;
+	//		printf("in main: %d %d \n",p->i,p->j);
+			/* Now create the thread passing it data as a parameter */
+			pthread_attr_t attr; //Set of thread attributes
+			//Get the default attributes
+			pthread_attr_init(&attr);
+			//Create the thread
+			pthread_create(&id[cnt++], NULL, elementrunner, p);
+		}
+	}
+	for (int i = 0; i < n*m; ++i) {
+		pthread_join(id[i], NULL);
+	}
+}
 int main(void) {
 	initialize();
 	nonThreadedMatMult();
-	printf("%d\n",sizeof(b));
-	printMatrix(a,n,m);
-	printMatrix(b,m,r);
-	printMatrix(c,n,r);
+	printMatrix(a, n, m);
+	printMatrix(b, m, r);
+	printMatrix(c, n, r);
+	threadedMatMultPerElement();
+	printMatrix(d, n, r);
 	return EXIT_SUCCESS;
 }
