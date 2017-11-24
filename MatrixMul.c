1332e20856635d5a8a8e31f3ac051afd3b24b4ac
moh4med <moh4med.ahmed@gmail.com>
2017-11-24 14:33:36 +0200

    add threaded matrix multiplicatio per row

--- MatrixMul.c
+++ MatrixMul.c
@@ -1,7 +1,6 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-int **a, **b, **c, **d;
+int **a, **b, **c, **d, **e;
 int n, m, r;
 #define MAXSIZE 10
 #define MAXELEMENT 5
@@ -39,6 +38,10 @@ void initialize() {
 	for (int i = 0; i < n; ++i) {
 		d[i] = malloc(r * sizeof(int));
 	}
+	e = malloc(n * sizeof(int*));
+	for (int i = 0; i < n; ++i) {
+		e[i] = malloc(r * sizeof(int));
+	}
 
 }
 void nonThreadedMatMult() {
@@ -61,36 +64,51 @@ void printMatrix(int **x, int n, int m) {
 	printf("\n");
 }
 void elementrunner(void *param) {
-	 struct data *p = param; // the structure that holds our data
-	   int sum = 0;
-	   //Row multiplied by column
-	   for (int k = 0; k < m; ++k) {
+	struct data *p = param; // the structure that holds our data
+	int sum = 0;
+	//Row multiplied by column
+	for (int k = 0; k < m; ++k) {
 		//printf("in thread:%d %d \n",p->i,p->j);
-		sum+=(a[p->i][k]*b[k][p->j]);
-	   }
-	   //assign the sum to its coordinate
-	   d[p->i][p->j] = sum;
-	   //Exit the thread
-	   pthread_exit(0);
+		sum += (a[p->i][k] * b[k][p->j]);
+	}
+	//assign the sum to its coordinate
+	d[p->i][p->j] = sum;
+	//Exit the thread
+	pthread_exit(0);
 }
 void threadedMatMultPerElement() {
-	pthread_t id[n*r];
-	int cnt=0;
+	pthread_t id[n * r];
+	int cnt = 0;
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < r; ++j) {
 			struct data *p = (struct data *) malloc(sizeof(struct data));
 			p->i = i;
 			p->j = j;
-	//		printf("in main: %d %d \n",p->i,p->j);
-			/* Now create the thread passing it data as a parameter */
-			pthread_attr_t attr; //Set of thread attributes
-			//Get the default attributes
-			pthread_attr_init(&attr);
-			//Create the thread
+			//	printf("in main: %d %d \n",p->i,p->j);
 			pthread_create(&id[cnt++], NULL, elementrunner, p);
 		}
 	}
-	for (int i = 0; i < n*m; ++i) {
+	for (int i = 0; i < n * m; ++i) {
+		pthread_join(id[i], NULL);
+	}
+}
+void rowrunner(int row) {
+	for (int j = 0; j < r; ++j) {
+		e[row][j] = 0;
+		for (int k = 0; k < m; ++k) {
+			e[row][j] += (a[row][k] * b[k][j]);
+		}
+	}
+
+	pthread_exit(0);
+}
+void threadedMatMultPerRow() {
+	pthread_t id[n];
+	int cnt = 0;
+	for (int i = 0; i < n; ++i) {
+		pthread_create(&id[cnt++], NULL, rowrunner, i);
+	}
+	for (int i = 0; i < n; ++i) {
 		pthread_join(id[i], NULL);
 	}
 }
@@ -102,5 +120,7 @@ int main(void) {
 	printMatrix(c, n, r);
 	threadedMatMultPerElement();
 	printMatrix(d, n, r);
+	threadedMatMultPerRow();
+	printMatrix(e, n, r);
 	return EXIT_SUCCESS;
 }
